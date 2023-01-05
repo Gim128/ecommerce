ea6ecbf8dd705d9fd01028fa630e3e212873f3d1
ican-graphics-01 <lankaican@gmail.com>
2023-01-05 15:27:18 +0530

    vuejs ecommerce project initialize

---
+++ src/components/NavbarView.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app-bar app color="white" flat>
+    <v-badge color="#D5F0DB" dot>
+        <v-img src="1.png" contain width="30"></v-img>
+    </v-badge>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+    name: "NavbarView"
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <navbar-view />
+    <v-container fluid>
+      
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+import NavbarView from '@/components/NavbarView.vue'
+export default {
+    name: "HomeViewComponent",
+
+    components: {
+      NavbarView,
+    }
+        
+}
+</script>
+
+<style>
+
+</style>
